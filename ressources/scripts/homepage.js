29e00969c6b702ec6b6095debdd3c818bf9e6d4d
Romain DAUNAS <romain.daunas@gmail.com>
2021-06-07 15:23:51 +0200

    homepage interaction first implementation

--- ressources/scripts/homepage.js
+++ ressources/scripts/homepage.js
@@ -1,84 +1,93 @@
-import {Photograph} from'./photographe';
+import {Photograph} from'./photographe.js';
 
 
-const photograhContainer = document.querySelector(".homepage__content");
+const photographContainer = document.querySelector(".homepage__content");
 
 //Store data de tout les photographes
-let photograhData = getAllPhotograph();
+let photograhData= [];
 //Store les data des photographes à afficher
 let photographList = [];
+getAllPhotograph();
 
-//TODO              function calls
-//Filter proxy 
-function filtering(filter) {
-    switch(filter) {
-
-        case "portrait":
-            break;
-        case "art":
-            break;
-        case "fashion":
-            break;
-        case "architecture":
-            break;
-        case "travel":
-            break;
-        case "sport":
-            break;
-        case "animals":
-            break;
-        case "events":
-            break;
-        default :
-            break;
-    }    
+//Event Listener on filters
+let filters = document.querySelectorAll(".filter");
+for(let filterElement of filters){
+    filterElement.addEventListener('click', () => {
+        filtering(filterElement.innerText.toLowerCase())
+    })
 }
 
-//TODO                  display hidden || Remove
+//Filter proxy 
+function filtering(filter) {  
+    photographeFiltering(filter);
+    listRender();
+}
+
 //Render photograh
 function listRender() {
     //delete all photograph
+    for(let photographs of document.querySelectorAll('.card')) {
+        photographs.remove();
+    }
 
     //if no filter 
     if(photographList.length == 0) {
         //render all photograph
-         photograhContainer.innerHTML();
+        photographList = photograhData; 
     }
-    //render photograph from photographList
-    for (let photograh in photographList) {
-        photograhContainer.innerHTML();
-    }
+    //rendering photographers
     
+    for(let photograph of photographList){
+
+        //Extracting tags and prerendering html
+        let photographTagRender ="";
+        for(let tag of photograph.tags) {
+            photographTagRender +=`<a class="filter">#${tag}</a>`;
+        }      
+        photographContainer.insertAdjacentHTML("beforeend",`
+            <div class="card">
+            <a href="#" class="card__link">
+                <img class="card__image" src="./ressources/images/Photographers ID Photos/${photograph._portrait}"/>
+                <h2 class="card__title">${photograph._name}</h2>
+            </a>            
+            <p class="card__location">${photograph.city}, ${photograph._country}</p>
+            <p class="card__bio"${photograph._tagline}</p>
+            <p class="card__price">${photograph._price}€/jour</p>
+            <div class="card__tags">${photographTagRender}</div>
+            </div>
+        `); 
+    }
 
 }
-
+//TODO
 // filter all photograph into a list matching filter tag
 function photographeFiltering(filter) {
     //check each photograph
     photographList = photograhData.filter( (photograph) => {
         //check photograph tag
-        for (let tag in photograph.tags) {
+        for (let tag of photograph.tags) {
             //check tag match filter
-            if(tag == filter){
+            if(tag == filter.substring(1)){
+                
                 return true;
             }
-            return false;
         }
-        
+        return false;        
     })
+    console.log(photographList);
 } 
 
-//TODO              Instantiate using JSON
 //loop through json to instantiate Photograh object
 function getAllPhotograph()  {
-    let allPhotograph =[];
-    //populate with data from json
-    allPhotograph.push(new Photograph());
-    allPhotograph.push(new Photograph());
-    allPhotograph.push(new Photograph());
-    allPhotograph.push(new Photograph());
-    allPhotograph.push(new Photograph());
-    allPhotograph.push(new Photograph());
-
-    return allPhotograph;
+    let allPhotograph= [];
+    fetch("./ressources/data/FishEyeData.json")
+    .then(response => response.json())  //transforme la reponse en json
+    .then(data => data.photographers)   //extrait les photographes des données json
+    .then( (jsonData) => {              //crée un objet Photograph pour chaque photograh des données json
+        for (let i=0;i < jsonData.length; i++){
+            allPhotograph.push(new Photograph(jsonData[i].name , jsonData[i].id , jsonData[i].city , jsonData[i].country , jsonData[i].tags , jsonData[i].tagline , jsonData[i].price , jsonData[i].portrait))
+        }
+        photograhData = allPhotograph;
+        listRender();
+    });
 }
--- ressources/scripts/photographe.js
+++ ressources/scripts/photographe.js
@@ -1,39 +1,63 @@
-class Photograph {
+export class Photograph {
+
 
     constructor(name, id, city, country, tags, tagline, price, portrait){
-        this.name = name;
-        this.id = id;
-        this.city = city;
-        this.country = country;
-        this.tags = tags;
-        this.tagline = tagline;
-        this.price = price;
-        this.portrait = portrait;
+        this._name = name;
+        this._id = id;
+        this._city = city;
+        this._country = country;
+        this._tags = tags;
+        this._tagline = tagline;
+        this._price = price;
+        this._portrait = portrait;
     }
 
     get name() {
-        return this.name;
+        return this._name;
     }
     get id() {
-        return this.id;
+        return this._id;
     }
     get city() {
-        return this.city;
+        return this._city;
     }
     get country() {
-        return this.country;
+        return this._country;
     }
     get tags() {
-        return this.tags;
+        return this._tags;
     }
     get tagline() {
-        return this.tagline;
+        return this._tagline;
     }
     get price() {
-        return this.price;
+        return this._price;
     }
     get portrait() {
-        return this.portrait;
-    }    
+        return this._portrait;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    set id(value) {
+        this._id = value;
+    } 
+    set city(value) {
+        this._city = value;
+    } 
+    set country(value) {
+        this._country = value;
+    } 
+    set tags(value) {
+        this._tags = value;
+    } 
+    set tagline(value) {
+        this._tagline = value;
+    } 
+    set price(value) {
+      this._price = value;
+    } 
+    set portrait(value) {
+       this._portrait = value;
+    }     
 }
-export default Photograph;
