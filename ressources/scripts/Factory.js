b4b2d9b1c2a8ec75d755bd96eacfe4051f8ab582
Romain DAUNAS <romain.daunas@gmail.com>
2021-07-24 19:23:52 +0200

    Alt text

--- ressources/scripts/Factory.js
+++ ressources/scripts/Factory.js
@@ -5,9 +5,9 @@ export class Factory {
 
     static createMedia(mediaInfo) {
         if("video" in mediaInfo) {
-            return new Video(mediaInfo.id, mediaInfo.photographerId, mediaInfo.title , mediaInfo.video, mediaInfo.tags , mediaInfo.likes, mediaInfo.date, mediaInfo.price, mediaInfo.alt);
+            return new Video(mediaInfo.id, mediaInfo.photographerId, mediaInfo.title , mediaInfo.video, mediaInfo.tags , mediaInfo.likes, mediaInfo.date, mediaInfo.price, mediaInfo.alt-text);
         }else {
-            return new Photo(mediaInfo.id, mediaInfo.photographerId, mediaInfo.title , mediaInfo.image, mediaInfo.tags , mediaInfo.likes, mediaInfo.date, mediaInfo.price, mediaInfo.alt)
+            return new Photo(mediaInfo.id, mediaInfo.photographerId, mediaInfo.title , mediaInfo.image, mediaInfo.tags , mediaInfo.likes, mediaInfo.date, mediaInfo.price, mediaInfo.alt-text)
         }
     }
 }
